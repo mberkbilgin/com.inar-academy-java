6020319abc75f5b9e131c8d0da73657851d15c8f
M.Berk Bilgin <m.berkbilgin@outlook.com>
2022-10-01 18:09:32 +0300

    reverseWords

--- src/chapters/chapter06/Exercises/Exercise06_06.java
+++ src/chapters/chapter06/Exercises/Exercise06_06.java
@@ -1,2 +1,24 @@
-package chapters.chapter06.Exercises;public class Exercise06_06 {
+package chapters.chapter06.Exercises;
+
+import java.util.Scanner;
+
+public class Exercise06_06 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter an integer:");
+        int i = input.nextInt();
+        displayPattern(i);
+
+    }
+    public static void displayPattern(int n){
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < n - i ; j++) {
+                System.out.print("    ");
+            }
+            for (int j = i; j >= 1 ; j--) {
+                System.out.printf("%-4d",j);
+            }
+            System.out.println();
+        }
+    }
 }
--- src/chapters/chapter06/Exercises/Exercise06_07.java
+++ src/chapters/chapter06/Exercises/Exercise06_07.java
@@ -1,2 +1,26 @@
-package chapters.chapter06.Exercises;public class Exercise06_07 {
+package chapters.chapter06.Exercises;
+
+import java.util.Scanner;
+
+public class Exercise06_07 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter an investment amount:");
+        double amount = input.nextDouble();
+        System.out.println("Enter annual interest rate in percentage: ");
+        double annualRate = input.nextDouble();
+        double monthlyRate = annualRate / 1200;
+        System.out.println("Enter number of years: ");
+        double years = input.nextDouble();
+        System.out.println("Years     Future Value");
+        for (int i = 1; i <= years; i++) {
+            System.out.printf("%-4d",i);
+            System.out.printf("%18.2f\n", futureInvestmentValue(amount, monthlyRate, i));
+        }
+
+    }
+    public static double futureInvestmentValue(double amount, double monthlyRate, double years){
+        double futureValue = amount *  Math.pow(1 + monthlyRate, years * 12);
+        return futureValue;
+    }
 }
