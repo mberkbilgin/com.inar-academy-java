040c5035599e9b63407dddc76387e3ae8281cc04
M.Berk Bilgin <m.berkbilgin@outlook.com>
2022-10-04 14:20:16 +0300

    Exercise06_23

--- src/chapters/chapter06/Exercises/Exercise06_23.java
+++ src/chapters/chapter06/Exercises/Exercise06_23.java
@@ -1,4 +1,28 @@
 package chapters.chapter06.Exercises;
 
+import java.util.Scanner;
+
 public class Exercise06_23 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String str = input.nextLine();
+        System.out.println("Enter a char: ");
+        String ch = input.next();
+        while(ch.length() != 1){
+            System.out.println("Try again. Should be one character.");
+            ch = input.next();
+        }
+        char ch1 = ch.charAt(0);
+        System.out.println("Occurrences of "+ch1+" in the sentence is "+count(str,ch1) );
+    }
+    public static int count(String str, char a){
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) == a){
+                count++;
+            }
+        }
+        return count;
+    }
 }
