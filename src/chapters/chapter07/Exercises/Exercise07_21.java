2acedab83aa4fade875e0f67d8297cae9b052e29
M.Berk Bilgin <m.berkbilgin@outlook.com>
2022-10-14 13:01:10 +0300

    Exercise07_20

---
+++ src/chapters/chapter07/Exercises/Exercise07_21.java
@@ -0,0 +1,15 @@
+package chapters.chapter07.Exercises;
+
+import java.util.Scanner;
+
+public class Exercise07_21 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number of balls:");
+        int numberOfBalls = input.nextInt();
+        System.out.println("Enter the number of slots:");
+        int numberOfSlots = input.nextInt();
+        String[] slots = new String[numberOfSlots - 1];
+        beanMachine()
+    }
+}
