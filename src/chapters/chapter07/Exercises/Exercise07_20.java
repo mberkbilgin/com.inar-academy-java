03652bef89152817dfbea32e29d7ad2796ed0d39
M.Berk Bilgin <m.berkbilgin@outlook.com>
2022-10-14 13:00:51 +0300

    Exercise07_20

---
+++ src/chapters/chapter07/Exercises/Exercise07_20.java
@@ -0,0 +1,41 @@
+package chapters.chapter07.Exercises;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Exercise07_20 {
+    public static void main(String[] args) {
+        double[] numbers = new double[10];
+        System.out.println("Enter ten numbers:");
+        fillTheArray(numbers);
+        System.out.println("Before the sort");
+        System.out.println(Arrays.toString(numbers));
+        sortTheArray(numbers);
+        System.out.println("After the sort");
+        System.out.println(Arrays.toString(numbers));
+    }
+
+    public static void sortTheArray(double[] numbers) {
+        for (int i = numbers.length - 1; i >= 1; i--) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (numbers[i] < numbers[j]) {
+                    swap(numbers, i, j);
+                }
+
+            }
+        }
+    }
+
+    public static void swap(double[] numbers, int i, int j) {
+        double temp = numbers[i];
+        numbers[i] = numbers[j];
+        numbers[j] = temp;
+    }
+
+    public static void fillTheArray(double[] numbers) {
+        Scanner input = new Scanner(System.in);
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = input.nextInt();
+        }
+    }
+}
