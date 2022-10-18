1c3c15d409af57cbc6a0ce1667879433f1c219a8
M.Berk Bilgin <m.berkbilgin@outlook.com>
2022-10-18 20:11:13 +0300

    Exercise07_34

---
+++ src/chapters/chapter07/Exercises/Exercise07_34.java
@@ -0,0 +1,30 @@
+package chapters.chapter07.Exercises;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Exercise07_34 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a string for sort:");
+        String str = input.nextLine();
+        System.out.println(sort(str));
+
+    }
+    public static String sort(String s){
+        char[] arr = new char[s.length()];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = s.charAt(i);
+        }
+        for (int i = 0; i < s.length() - 1; i++) {
+            for (int j = i + 1; j < s.length(); j++) {
+                if (arr[i] > arr[j]){
+                    char temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        return Arrays.toString(arr);
+    }
+}
