5eb45a706da5396b260848108f8f835ae4118745
M.Berk Bilgin <m.berkbilgin@outlook.com>
2022-10-18 20:51:30 +0300

    Checkpoint08_03

--- src/chapters/chapter08/Checkpoints/Checkpoint08_01.java
+++ src/chapters/chapter08/Checkpoints/Checkpoint08_01.java
@@ -1,2 +1,21 @@
-package chapters.chapter08.Checkpoints;public class Checkpoint08_01 {
+package chapters.chapter08.Checkpoints;
+
+import java.util.Arrays;
+
+public class Checkpoint08_01 {
+    public static void main(String[] args) {
+        int[][] twoDimensionalArray = new int[4][5];
+        fillArr(twoDimensionalArray);
+
+    }
+
+    public static void fillArr(int[][] twoDimensionalArray) {
+        for (int i = 0; i <twoDimensionalArray.length ; i++) {
+            for (int j = 0; j <twoDimensionalArray[i].length ; j++) {
+                twoDimensionalArray[i][j] = (int)(Math.random()*10);
+                System.out.print(twoDimensionalArray[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
