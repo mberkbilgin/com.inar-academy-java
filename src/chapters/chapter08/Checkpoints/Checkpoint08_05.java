1ee1223481374c742ffbe00bf6e45651e658135a
M.Berk Bilgin <m.berkbilgin@outlook.com>
2022-10-19 20:29:16 +0300

    Checkpoint08_07

--- src/chapters/chapter08/Checkpoints/Checkpoint08_05.java
+++ src/chapters/chapter08/Checkpoints/Checkpoint08_05.java
@@ -1,2 +1,12 @@
-package chapters.chapter08.Checkpoints;public class Checkpoint08_05 {
+package chapters.chapter08.Checkpoints;
+
+public class Checkpoint08_05 {
+    public static void main(String[] args) {
+        int[][] array = {{1, 2}, {3, 4}, {5, 6}};
+        for (int i = array.length - 1; i >= 0; i--) {
+            for (int j = array[i].length - 1; j >= 0; j--)
+                System.out.print(array[i][j] + " ");
+            System.out.println();
+        }
+    }
 }
