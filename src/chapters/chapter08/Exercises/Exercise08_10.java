a7b03adfc42ede74b5aeac5de7a041d1bf975953
M.Berk Bilgin <m.berkbilgin@outlook.com>
2022-10-22 11:21:29 +0300

    Exercise08_10

---
+++ src/chapters/chapter08/Exercises/Exercise08_10.java
@@ -0,0 +1,57 @@
+package chapters.chapter08.Exercises;
+
+public class Exercise08_10 {
+    public static void main(String[] args) {
+        int[][] arr = new int[4][4];
+        fillTheArr(arr);
+        System.out.println("The largest row index: " + numberOfOneForRow(arr));
+        System.out.println("The largest column index: " + numberOfOneForColumn(arr));
+
+    }
+
+    public static int numberOfOneForColumn(int[][] arr) {
+        int highCount = Integer.MIN_VALUE;
+        int index = -1;
+        for (int i = 0; i < arr.length; i++) {
+            int count = 0;
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[j][i] == 1) {
+                    count++;
+                }
+            }
+            if (count > highCount) {
+                highCount = count;
+                index = i;
+            }
+        }
+        return index;
+    }
+
+    public static int numberOfOneForRow(int[][] arr) {
+        int highCount = Integer.MIN_VALUE;
+        int index = -1;
+        for (int i = 0; i < arr.length; i++) {
+            int count = 0;
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] == 1) {
+                    count++;
+                }
+            }
+            if (count > highCount) {
+                highCount = count;
+                index = i;
+            }
+        }
+        return index;
+    }
+
+    public static void fillTheArr(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = (int) (Math.random() * 2);
+                System.out.print(arr[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
