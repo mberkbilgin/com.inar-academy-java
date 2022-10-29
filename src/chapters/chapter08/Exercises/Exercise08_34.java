c30dea8c5818cd4f907900374907cb34ee61034b
M.Berk Bilgin <m.berkbilgin@outlook.com>
2022-10-29 10:36:11 +0300

    modift

--- src/chapters/chapter08/Exercises/Exercise08_34.java
+++ src/chapters/chapter08/Exercises/Exercise08_34.java
@@ -1,2 +1,47 @@
-package chapters.chapter08.Exercises;public class Exercise08_34 {
+package chapters.chapter08.Exercises;
+
+import java.util.Scanner;
+
+public class Exercise08_34 {
+    public static void main(String[] args) {
+        double[][] points = new double[6][2];
+        System.out.println("Enter 6 points:");
+        fillTheArr(points);
+        display(getRightmostLowestPoint(points));
+    }
+
+    public static void display(double[] rightmostLowestPoint) {
+        System.out.println("The rightmost lowest point is (" + rightmostLowestPoint[0] + ", " + rightmostLowestPoint[1] + ")");
+    }
+
+    public static double[] getRightmostLowestPoint(double[][] points) {
+        double[] arr = new double[2];
+        int index = -1;
+        double min = Integer.MAX_VALUE;
+        for (int i = 0; i < points.length ; i++) {
+            if(points[i][1] < min){
+                min = points[i][1];
+                index = i;
+            }else if(points[i][1] == min){
+                if(points[i][0] > points[i - 1][0] ){
+                    index = i;
+                }else{
+                    index = i -1;
+                }
+            }
+        }
+        arr[0] = points[index][0];
+        arr[1] = points[index][1];
+        return arr;
+    }
+
+    public static void fillTheArr(double[][] points) {
+        Scanner input = new Scanner(System.in);
+        for (int i = 0; i < points.length; i++) {
+            for (int j = 0; j < points[i].length; j++) {
+                points[i][j] = input.nextDouble();
+            }
+        }
+    }
+
 }
--- src/chapters/chapter08/Exercises/Exercise08_35.java
+++ src/chapters/chapter08/Exercises/Exercise08_35.java
@@ -1,2 +1,59 @@
-package chapters.chapter08.Exercises;public class Exercise08_35 {
+package chapters.chapter08.Exercises;
+
+import java.util.Scanner;
+
+public class Exercise08_35 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number of rows in the matrix:");
+        int number = input.nextInt();
+        int[][] matrix = new int[number][number];
+        System.out.println("Enter the matrix row-by-row:");
+        fillTheArr(matrix);
+        int[] result = largestBlock(matrix);
+        System.out.println("The maximum square submatrix is at (" + result[0] + ", " + result[1] + ") with size " + result[2]);
+
+    }
+
+    public static int[] largestBlock(int[][] matrix) {
+        int[] result = new int[3];
+        int max = 2;
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                while(isSquare(row,col,max,matrix)){
+                    result[0] = row;
+                    result[1] = col;
+                    result[2] = max;
+                    max++;
+                }
+            }
+        }
+        return result;
+    }
+
+    public static boolean isSquare(int row, int col, int max, int[][] matrix) {
+        if(row + max > matrix.length){
+            return false;
+        }
+        if (col + max > matrix.length){
+            return false;
+        }
+        for (int i = row; i < row + max; i++) {
+            for (int j = col; j < col + max; j++) {
+                if(matrix[i][j] != 1){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void fillTheArr(int[][] matrix) {
+        Scanner input = new Scanner(System.in);
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length ; j++) {
+                matrix[i][j] = input.nextInt();
+            }
+        }
+    }
 }
