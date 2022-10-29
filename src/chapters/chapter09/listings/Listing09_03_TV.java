684a2da11956362c218ea1b2e7b515db8110b42e
M.Berk Bilgin <m.berkbilgin@outlook.com>
2022-10-29 13:43:38 +0300

    Listing09_03

---
+++ src/chapters/chapter09/listings/Listing09_03_TV.java
@@ -0,0 +1,11 @@
+package chapters.chapter09.listings;
+
+public class Listing09_03_TV {
+        int channel = 1;
+        int volumeLevel = 1;
+        boolean on = false;
+        public  Listing09_03_TV(){
+
+        }
+
+}
