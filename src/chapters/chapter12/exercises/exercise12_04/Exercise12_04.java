aa0f1ef17ec03765b220b21c9a553a528a0e02eb
M.Berk Bilgin <m.berkbilgin@outlook.com>
2022-11-25 10:08:04 +0300

    exercise12_04

---
+++ src/chapters/chapter12/exercises/exercise12_04/Exercise12_04.java
@@ -0,0 +1,73 @@
+package chapters.chapter12.exercises.exercise12_04;
+
+import java.util.Date;
+
+public class Exercise12_04 {
+    private double annualInterestRate;
+    private int numberOfYears;
+    private double loanAmount;
+    private java.util.Date loanDate;
+
+    public Exercise12_04() {
+        this(2.5, 1, 1000);
+    }
+
+    public Exercise12_04(double annualInterestRate, int numberOfYears, double loanAmount) {
+        if (loanAmount <= 0 || annualInterestRate <= 0 || numberOfYears <= 0) {
+            throw new IllegalArgumentException("Checkout your values, they must be higher than zero ");
+        }
+        this.annualInterestRate = annualInterestRate;
+        this.numberOfYears = numberOfYears;
+        this.loanAmount = loanAmount;
+        this.loanDate = new Date();
+    }
+
+    public double getAnnualInterestRate() {
+        return annualInterestRate;
+    }
+
+    public void setAnnualInterestRate(double annualInterestRate) {
+        this.annualInterestRate = annualInterestRate;
+    }
+
+    public int getNumberOfYears() {
+        return numberOfYears;
+    }
+
+    public void setNumberOfYears(int numberOfYears) {
+        this.numberOfYears = numberOfYears;
+    }
+
+    public double getLoanAmount() {
+        return loanAmount;
+    }
+
+    public void setLoanAmount(double loanAmount) {
+        this.loanAmount = loanAmount;
+    }
+
+    public Date getLoanDate() {
+        return loanDate;
+    }
+
+    public double getMonthlyPayment() {
+        double monthlyInterestRate = annualInterestRate / 1200;
+        double monthlyPayment = loanAmount * monthlyInterestRate / (1 - (1 / Math.pow(1 + monthlyInterestRate, numberOfYears * 12)));
+        return monthlyPayment;
+    }
+
+    public double getTotalPayment() {
+        double totalPayment = getMonthlyPayment() * numberOfYears * 12;
+        return totalPayment;
+    }
+
+    @Override
+    public String toString() {
+        return "Annual Interest Rate = " + getAnnualInterestRate()
+                + "\nMonthly Payment =  " + getMonthlyPayment()
+                + "\nTotal Payment = " + getTotalPayment()
+                + "\nThe Date = " + getLoanDate()
+                + "\n_______________________________________________"
+                + "\n_______________________________________________";
+    }
+}
---
+++ src/chapters/chapter12/exercises/exercise12_04/Test.java
@@ -0,0 +1,12 @@
+package chapters.chapter12.exercises.exercise12_04;
+
+public class Test {
+    public static void main(String[] args) {
+        Exercise12_04 loan1 = new Exercise12_04();
+        Exercise12_04 loan2 = new Exercise12_04(41, 8, 48651);
+        //Exercise12_04 loan3 = new Exercise12_04(0,-5,46168);
+        System.out.println(loan1);
+        System.out.println(loan2);
+        //System.out.println(loan3);
+    }
+}
