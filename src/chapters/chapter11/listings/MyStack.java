0ed7f8d2430eff002f63af6dd760a9bc276b8508
M.Berk Bilgin <m.berkbilgin@outlook.com>
2022-11-29 23:58:18 +0300

    modify

--- src/chapters/chapter11/listings/MyStack.java
+++ src/chapters/chapter11/listings/MyStack.java
@@ -1,2 +1,29 @@
-package chapters.chapter11.listings;public class MyStack {
+package chapters.chapter11.listings;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class MyStack {
+    private ArrayList<Object> list = new ArrayList<>();
+    public boolean isEmpty(){
+        return list.isEmpty();
+    }
+    public int getSize(){
+        return list.size();
+    }
+    public Object peek(){
+        return list.get(getSize() - 1);
+    }
+    public Object pop(){
+        Object o = list.get(getSize() - 1);
+        list.remove(getSize() - 1);
+        return o;
+    }
+    public void push(Object o){
+        list.add(o);
+    }
+    @Override
+    public String toString(){
+        return "stack: " + list.toString();
+    }
 }
