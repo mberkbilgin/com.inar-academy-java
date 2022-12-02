aa8abbd9b77ae8f4a6810949511606bd81b7dbdc
M.Berk Bilgin <m.berkbilgin@outlook.com>
2022-12-02 15:48:37 +0300

    update

--- src/chapters/chapter13/listings/Rational.java
+++ src/chapters/chapter13/listings/Rational.java
@@ -1,2 +1,107 @@
-package chapters.chapter13.listings;public class Rational {
+package chapters.chapter13.listings;
+
+public class Rational extends Number implements Comparable<Rational> {
+    private long numerator = 0;
+    private long denominator = 1;
+
+    public Rational() {
+        this(0, 1);
+    }
+
+    public Rational(long numerator, long denominator) {
+        long gcd = gcd(numerator, denominator);
+        this.numerator = ((denominator > 0) ? 1 : -1) * numerator / gcd;
+        this.denominator = Math.abs(denominator) / gcd;
+    }
+
+    private static long gcd(long numerator, long denominator) {
+        long n1 = Math.abs(numerator);
+        long n2 = Math.abs(denominator);
+        int gcd = 1;
+        for (int i = 1; i <= n1 && i <= n2; i++) {
+            if (n1 % i == 0 && n2 % i == 0) {
+                gcd = i;
+            }
+        }
+        return gcd;
+    }
+
+    public long getNumerator() {
+        return numerator;
+    }
+
+    public long getDenominator() {
+        return denominator;
+    }
+
+    public Rational add(Rational secondRational) {
+        long n = numerator * secondRational.getDenominator() +
+                denominator * secondRational.getNumerator();
+        long d = denominator * secondRational.getDenominator();
+        return new Rational(n, d);
+    }
+
+    public Rational subtract(Rational secondRational) {
+        long n = numerator * secondRational.getDenominator()
+                - denominator * secondRational.getNumerator();
+        long d = denominator * secondRational.getDenominator();
+        return new Rational(n, d);
+    }
+
+    public Rational multiply(Rational secondRational) {
+        long n = numerator * secondRational.getNumerator();
+        long d = denominator * secondRational.getDenominator();
+        return new Rational(n, d);
+    }
+
+    public Rational divide(Rational secondRational) {
+        long n = numerator * secondRational.getDenominator();
+        long d = denominator * secondRational.numerator;
+        return new Rational(n, d);
+    }
+
+    @Override
+    public String toString() {
+        if (denominator == 1)
+            return numerator + "";
+        else
+            return numerator + "/" + denominator;
+    }
+
+    public boolean equals(Object other) {
+        if ((this.subtract((Rational) (other))).getNumerator() == 0)
+            return true;
+        else
+            return false;
+    }
+
+    @Override
+    public int compareTo(Rational o) {
+        if (this.subtract(o).getNumerator() > 0)
+            return 1;
+        else if (this.subtract(o).getNumerator() < 0)
+            return -1;
+        else
+            return 0;
+    }
+
+    @Override
+    public int intValue() {
+        return (int) doubleValue();
+    }
+
+    @Override
+    public long longValue() {
+        return (long) doubleValue();
+    }
+
+    @Override
+    public float floatValue() {
+        return (float) doubleValue();
+    }
+
+    @Override
+    public double doubleValue() {
+        return numerator * 1.0 / denominator;
+    }
 }
